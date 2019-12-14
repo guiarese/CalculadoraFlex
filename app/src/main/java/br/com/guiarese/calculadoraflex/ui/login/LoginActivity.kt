38096eb0bb0212b0fb14d70cccb494112529f374
Logon Aluno <logonaluno@local.com>
2019-12-14 14:55:54 -0300

    gar

--- app/src/main/java/br/com/guiarese/calculadoraflex/ui/login/LoginActivity.kt
+++ app/src/main/java/br/com/guiarese/calculadoraflex/ui/login/LoginActivity.kt
@@ -1,13 +1,38 @@
 package br.com.guiarese.calculadoraflex.ui.login
 
+import android.app.Activity
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import br.com.guiarese.calculadoraflex.R
+import br.com.guiarese.calculadoraflex.ui.signup.SignUpActivity
+import kotlinx.android.synthetic.main.activity_login.*
 
 class LoginActivity : AppCompatActivity() {
+
+    val NEW_USER_REQUEST = 1
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_login)
+        btSignup.setOnClickListener({
+            val criarConta = Intent(this, SignUpActivity::class.java)
+            startActivityForResult(criarConta, NEW_USER_REQUEST)
+        })
+    }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+        when(requestCode){
+            NEW_USER_REQUEST -> {
+                when(resultCode){
+                    Activity.RESULT_OK -> {
+                        inputLoginEmail
+                            .setText(data?.getStringExtra("email"))
+                    }
+                }
+            }
+            else -> {}
+        }
     }
 }
