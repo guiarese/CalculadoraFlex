0d25ede3d4dcc21027933659490ed93c25441ec0
Logon Aluno <logonaluno@local.com>
2019-12-14 16:05:05 -0300

    gar

--- app/src/main/java/br/com/guiarese/calculadoraflex/ui/form/FormActivity.kt
+++ app/src/main/java/br/com/guiarese/calculadoraflex/ui/form/FormActivity.kt
@@ -1,13 +1,28 @@
 package br.com.guiarese.calculadoraflex.ui.form
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import br.com.guiarese.calculadoraflex.R
+import br.com.guiarese.calculadoraflex.ui.result.ResultActivity
+import br.com.guiarese.calculadoraflex.ui.watchers.DecimalTextWatcher
+import kotlinx.android.synthetic.main.activity_form.*
 
 class FormActivity : AppCompatActivity() {
-
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_form)
+        etGasPrice.addTextChangedListener(DecimalTextWatcher(etGasPrice))
+        etEthanolPrice.addTextChangedListener(DecimalTextWatcher(etEthanolPrice))
+        etGasAverage.addTextChangedListener(DecimalTextWatcher(etGasAverage, 1))
+        etEthanolAverage.addTextChangedListener(DecimalTextWatcher(etEthanolAverage, 1))
+        btCalculate.setOnClickListener {
+            val proximatela = Intent(this@FormActivity, ResultActivity::class.java)
+            proximatela.putExtra("GAS_PRICE", etGasPrice.text.toString().toDouble())
+            proximatela.putExtra("ETHANOL_PRICE", etEthanolPrice.text.toString().toDouble())
+            proximatela.putExtra("GAS_AVERAGE", etGasAverage.text.toString().toDouble())
+            proximatela.putExtra("ETHANOL_AVERAGE", etEthanolAverage.text.toString().toDouble())
+            startActivity(proximatela)
+        }
     }
 }
